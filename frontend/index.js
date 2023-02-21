b61a661a74be0a66f4d81534cfd10d558301328d
Casey Bailey <soniczentropy@gmail.com>
2023-02-21 00:17:48 -0600

    Intro Done

--- frontend/index.js
+++ frontend/index.js
@@ -6,12 +6,15 @@ function Footer() {
 	return (<h3>Copyright Github Copilot</h3>);
 }
 
+let foo = (
+	<div className="fooClass">IM JSX FOLKS</div>
+);
+console.log(foo);
+
+
 const rootContainer = document.getElementById('root');
 const root = ReactDOM.createRoot(rootContainer);
 root.render(
-	<div>
-		<Greeting/>
-		<Footer/>
-	</div>
+	foo
 );
 
