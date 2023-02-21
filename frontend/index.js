e3cf1dd5d8367d470945c8b4a8011b1d37420a1d
Casey Bailey <soniczentropy@gmail.com>
2023-02-20 23:27:02 -0600

    Basic react render

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,5 @@
-ReactDOM.render(<h1>Hi pet daters</h1>);
 
-
+const rootContainer = document.getElementById('root');
+// createRoot(container!) in typescript
+const root = ReactDOM.createRoot(rootContainer);
+root.render(<h1>Hi pet daters</h1>);
