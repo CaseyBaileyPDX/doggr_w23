0f7255a9c923f0c3d6391d661d66c3a183017fd7
Casey Bailey <soniczentropy@gmail.com>
2023-02-21 04:17:04 -0600

    cleanup done

--- frontend/src/Components.tsx
+++ frontend/src/Components.tsx
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export const Header = () => {
 	return (<div>
-			<h1>Doggr</h1>
-			<h3>Where your pets finds love(tm)</h3>
-			<br/>
-		</div>
+		<h1>Doggr</h1>
+		<h3>Where your pets finds love(tm)</h3>
+		<br/>
+	</div>
 	);
 };
 
-export const Users = () => {
+export const Users = () => { 
 	const [users, setUsers] = useState([]);
 
 	useEffect(() => {
@@ -28,7 +28,7 @@ export const Users = () => {
 			<h2>Users:</h2>
 			{
 				users ?
-					<ul>{users.map((user) => <li key={user.email.toString()}>{user.name} - {user.email}</li>)}</ul>
+					<ul>{users.map((user: {email: string, name: string}) => <li key={user.email.toString()}>{user.name} - {user.email}</li>)}</ul>
 					: null
 			}
 		</div>
